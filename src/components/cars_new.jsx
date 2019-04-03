6b24e6e509571f0c29dd9dfe0f543a392cedccac
Fabien Naour <fabiennaour@gmail.com>
2019-04-03 16:00:27 +0200

    final update

---
+++ src/components/cars_new.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+
+import Aside from '../components/aside';
+import { addCar } from '../actions';
+
+class CarsNew extends Component {
+  onSubmit = (values) => {
+    this.props.addCar(this.props.garage, values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render () {
+    const required = value => value ? undefined : 'Required'
+    const maxLength = max => value =>
+      value && value.length > max ? `Must be ${max} characters or less` : undefined
+    const maxLength5 = maxLength(5)
+
+    return [
+      <Aside key="aside" garage={this.props.garage}>
+        <Link to="/">Back to list</Link>
+      </Aside>,
+      <div key="add" className="form-container" style={{ backgroundImage: "url('/assets/images/form.jpg')"}}>
+        <div className="overlay"></div>
+
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div className="form-group">
+            <label htmlFor="InputBrand">Brand</label>
+            <Field  name="brand" type="text" placeholder="Aston Martin" component="input" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputModel">Model</label>
+            <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputOwner">Owner</label>
+            <Field name="owner" validate={[ required, maxLength5 ]} type="text" placeholder="James Bond" component="input" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputPlate">Plate</label>
+            <Field name="plate" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+          </div>
+          <button type="submit" disabled={this.props.pristine || this.props.submitting}>Add car</button>
+        </form>
+
+      </div>
+    ];
+  }
+};
+
+function mapStateToProps(state) {
+  return {
+    garage: state.garage
+  };
+}
+
+export default reduxForm({
+  form: 'newCarForm' // a unique identifier
+})(
+  connect(mapStateToProps, { addCar })(CarsNew)
+);
--- src/index.jsx
+++ src/index.jsx
@@ -6,23 +6,51 @@ import reduxPromise from 'redux-promise';
 import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
+import CarsIndex from './containers/cars_index';
+import CarsShow from './containers/cars_show';
+import CarsNew from './components/cars_new';
 
+import { reducer as formReducer } from 'redux-form';
+
+import carsReducer from "./reducers/cars_reducer";
 import '../assets/stylesheets/application.scss';
 
-const reducers = combineReducers({
+const identityReducer = (state = null) => state;
+
+const initialState = {
   // key: reducer
+  cars: [
+      // { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+      // { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+      // { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+      // { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+      ],
+  garage: "FAB"
+};
+
+const reducers = combineReducers({
+  cars: carsReducer,
+  garage: identityReducer,
+  form: formReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <Router history={history}>
-      <Switch>
-        TODO
-      </Switch>
+      <div className="view-container">
+        <Switch>
+          <Route path="/" exact component={CarsIndex} />
+          <Route path="/cars/new" exact component={CarsNew} />
+          <Route path="/cars/:id" component={CarsShow} />
+        </Switch>
+      </div>
     </Router>
   </Provider>,
   document.getElementById('root')
 );
+
+
